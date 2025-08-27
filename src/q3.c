bde2bc8381c78771f32eaae0a145e6c47a000b0d
sameera <s.upadhyaya.3348@westcliff.eda>
2025-08-27 18:35:36 +0545

    assignment completed

--- src/q3.c
+++ src/q3.c
@@ -1,8 +1,10 @@
 // Write a C program to prompt the user for their name and display a personalized greeting message.
 
-#include <stdio.h>
-
-int main() {
-    //Enter your code here
+ #include <stdio.h>
+ int main() {
+char name[20] ;
+printf("Enter your name:");
+scanf("%19s",name);
+printf("Hello, %s! Welcome to C programming!\n", name);
     return 0;
 }
