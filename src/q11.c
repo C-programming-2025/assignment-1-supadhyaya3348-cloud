5b2ce698c62f551d362468f359883564fb81b774
sameera <s.upadhyaya.3348@westcliff.eda>
2025-08-28 19:59:58 +0545

    second commit

--- src/q11.c
+++ src/q11.c
@@ -1,17 +1,13 @@
-
 #include <stdio.h>
 
 int main() {
-    int a, result;
-
+    int a;
     printf("Enter a value for a: ");
     scanf("%d", &a);
 
-    
-    result = (++a) * 3 - 10;
-
-    
-    printf("After incrementing a by 1, multiplying by 3, and subtracting 10, the result is: %d\n", result);
+    // Expression directly inside printf
+    printf("%d\n", (++a) * 3 - 10);
 
     return 0;
 }
+
