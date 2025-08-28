2a33b99fcd540e2ffe305bd970a3419cd7683d62
sameera <s.upadhyaya.3348@westcliff.eda>
2025-08-28 21:07:25 +0545

    final commit

--- src/q24.c
+++ src/q24.c
@@ -6,7 +6,7 @@ int main() {
     printf("Enter a number: ");
     scanf("%d", &n);
 
-    // Expression: 1 if n is a perfect square, 0 otherwise
+   
     printf("%d\n", (sqrt(n) == (int)sqrt(n)));
 
     return 0;
