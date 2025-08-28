6b68497581dd2513d6c4ad484e3a825bdc8fcf83
sameera <s.upadhyaya.3348@westcliff.eda>
2025-08-28 20:42:29 +0545

    third commit

--- src/q17.c
+++ src/q17.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
-int main() {
-    char ch;
-
-    printf("Enter a character: ");
-    scanf("%c", &ch);
-
-    printf("%c is %s an uppercase letter.\n", ch, (ch >= 'A' && ch <= 'Z') ? "" : "not");
-
+int main()
+{
+    char a;
+    int checker;
+    printf("enter a character: ");
+    scanf("%c", &a);
+    checker = a >= 'A' && a <= 'Z';
+    printf("result: %d", checker);
     return 0;
 }
