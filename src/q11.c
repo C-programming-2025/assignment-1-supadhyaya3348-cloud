c2bb5210a26f2eb3d982bb467b030fcc615b1433
sameera <s.upadhyaya.3348@westcliff.eda>
2025-08-28 20:10:20 +0545

    second commit

--- src/q11.c
+++ src/q11.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
+
+#include <stdio.h>  
 
 int main() {
-    int a;
-    printf("Enter a value for a: ");
-    scanf("%d", &a);
-
-    // Perform operation directly in printf
-    printf("%d\n", (++a) * 3 - 10);
+    int x, result;
+    printf("Enter a number: ");
+    scanf("%d", &x);
+    result = (++x * 3) - 10;
+    printf("After incrementing by 1, multiplying by 3, and subtracting 10: %d\n", result);
 
     return 0;
 }
