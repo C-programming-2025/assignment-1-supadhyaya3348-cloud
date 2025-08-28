83d265d7399e3ee206f2a5c050637d37525c44df
sameera <s.upadhyaya.3348@westcliff.eda>
2025-08-28 21:04:15 +0545

    final commit

--- src/q20.c
+++ src/q20.c
@@ -7,7 +7,7 @@ int main() {
     scanf("%d", &n);
 
     
-    printf("%d is %s a multiple of 3 or 5.\n", n, (n % 3 == 0 || n % 5 == 0) ? "" : "not");
+    printf("%d", (n % 3 == 0 || n % 5 == 0) );
 
     return 0;
 }
